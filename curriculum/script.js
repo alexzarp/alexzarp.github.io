0111aa6ec5dc78b2564b1277fbd1b712b41af845
alexzarp <alexszds@gmail.com>
2024-01-21 13:26:34 -0300

    Atualização 21/01/2024

--- curriculum/script.js
+++ curriculum/script.js
@@ -8,3 +8,31 @@ function age(id) {
     }
     document.getElementById(id).textContent = age;
 }
+
+function exp(id) {
+    var birth = new Date(2019, 1, 1);
+    var today = new Date();
+    var exp = today.getFullYear() - birth.getFullYear();
+    document.getElementById(id).textContent = exp;
+}
+
+window.addEventListener('scroll', function () {
+    var button = document.getElementById('print');
+    var footer = document.getElementById('down');
+    var footerHeight = footer.offsetHeight;
+    var pageHeight = document.body.scrollHeight;
+    var scrollPosition = window.scrollY || window.scrollY || document.documentElement.scrollTop;
+    var windowHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+    var windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    if (windowWidth < 600) {
+        if (scrollPosition + windowHeight >= pageHeight - footerHeight) {
+            var offset = scrollPosition + windowHeight - (pageHeight - footerHeight);
+            button.style.bottom = (.5 - offset) + 'rem';
+        } else {
+            button.style.bottom = '.5rem';
+        }
+    } else {
+
+        button.style.bottom = '.5rem';
+    }
+});
