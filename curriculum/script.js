0cf72eb8433715a5c643988b5cd66595d9ee2699
alexzarp <alexszds@gmail.com>
2024-10-24 12:29:36 -0300

    Att

--- curriculum/script.js
+++ curriculum/script.js
@@ -1,38 +1,44 @@
 function age(id) {
-    var birth = new Date(2000, 10, 17);
-    var today = new Date();
-    var age = today.getFullYear() - birth.getFullYear();
-    var m = today.getMonth() - birth.getMonth();
-    if (m < 0 || (m === 0 && today.getDate() < birth.getDate())) {
-        age--;
-    }
-    document.getElementById(id).textContent = age;
+  var birth = new Date(2000, 9, 17);
+  var today = new Date();
+  var age = today.getFullYear() - birth.getFullYear();
+  var m = today.getMonth() - birth.getMonth();
+  if (m < 0 || (m === 0 && today.getDate() < birth.getDate())) {
+    age--;
+  }
+  document.getElementById(id).textContent = age;
 }
 
 function exp(id) {
-    var birth = new Date(2019, 1, 1);
-    var today = new Date();
-    var exp = today.getFullYear() - birth.getFullYear();
-    document.getElementById(id).textContent = exp;
+  var birth = new Date(2019, 0, 1);
+  var today = new Date();
+  var exp = today.getFullYear() - birth.getFullYear();
+  document.getElementById(id).textContent = exp;
 }
 
-window.addEventListener('scroll', function () {
-    var button = document.getElementById('print');
-    var footer = document.getElementById('down');
-    var footerHeight = footer.offsetHeight;
-    var pageHeight = document.body.scrollHeight;
-    var scrollPosition = window.scrollY || window.scrollY || document.documentElement.scrollTop;
-    var windowHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
-    var windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-    if (windowWidth < 600) {
-        if (scrollPosition + windowHeight >= pageHeight - footerHeight) {
-            var offset = scrollPosition + windowHeight - (pageHeight - footerHeight);
-            button.style.bottom = (.5 - offset) + 'rem';
-        } else {
-            button.style.bottom = '.5rem';
-        }
+window.addEventListener("scroll", function () {
+  var button = document.getElementById("print");
+  var footer = document.getElementById("down");
+  var footerHeight = footer.offsetHeight;
+  var pageHeight = document.body.scrollHeight;
+  var scrollPosition =
+    window.scrollY || window.scrollY || document.documentElement.scrollTop;
+  var windowHeight =
+    window.innerHeight ||
+    document.documentElement.clientHeight ||
+    document.body.clientHeight;
+  var windowWidth =
+    window.innerWidth ||
+    document.documentElement.clientWidth ||
+    document.body.clientWidth;
+  if (windowWidth < 600) {
+    if (scrollPosition + windowHeight >= pageHeight - footerHeight) {
+      var offset = scrollPosition + windowHeight - (pageHeight - footerHeight);
+      button.style.bottom = 0.5 - offset + "rem";
     } else {
-
-        button.style.bottom = '.5rem';
+      button.style.bottom = ".5rem";
     }
+  } else {
+    button.style.bottom = ".5rem";
+  }
 });
